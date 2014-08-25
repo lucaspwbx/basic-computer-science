fa141d24b3432b12ce7b604f8d321c56d9b20c84
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-25 12:17:28 -0300

    Refactoring

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -1,6 +1,6 @@
 package sorting
 
-func Bubblesort(list []int) []int {
+func BubbleSort(list []int) []int {
 	for i := len(list) - 1; i >= 1; i-- {
 		for j := 0; j < i; j++ {
 			if list[j] > list[j+1] {
@@ -11,4 +11,22 @@ func Bubblesort(list []int) []int {
 		}
 	}
 	return list
+}
+
+func SelectionSort(list []int) []int {
+  var i, j, min, aux int
+  for i = 0; i < len(list)-1; i++ {
+    min = i
+    for j = i+1; j < len(list); j++ {
+      if list[j] < list[min] {
+	min = j
+      }
+    }
+    if i != min {
+      aux = list[i]
+      list[i] = list[min]
+      list[min] = aux
+    }
+  }
+  return list
 }
--- sorting/sorting_test.go
+++ sorting/sorting_test.go
@@ -5,7 +5,19 @@ import "testing"
 func TestBubblesort(t *testing.T) {
 	expected := []int{26, 54, 93}
 	list := []int{54, 26, 93}
-	result := Bubblesort(list)
+	result := BubbleSort(list)
+
+	for k, v := range result {
+		if v != expected[k] {
+			t.Errorf("Error sorting")
+		}
+	}
+}
+
+func TestSelectionSort(t *testing.T) {
+	expected := []int{26, 54, 93}
+	list := []int{54, 26, 93}
+	result := SelectionSort(list)
 
 	for k, v := range result {
 		if v != expected[k] {
